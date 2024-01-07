b7b443a99fddac4dc98111e568a4064cb0f7da61
blackpanther26 <priyanshuchahal1@gmail.com>
2024-01-07 17:27:54 +0530

    adds banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar.js'
-import {Routes,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import {Banner} from './components/Banner';
+//import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <>
     <Navbar/>
+    <Banner/>
     </>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,71 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import { useState, useEffect } from "react";
+import headerImg from "../assets/images/header-img.jpg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to Secure V !</span>
+            <h1>
+              {`Hi I am Priyanshu Chahal`}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+              deleniti quo nisi veritatis ipsa deserunt ullam voluptatum, animi
+              architecto qui ad non magnam! Porro, facere unde! Odit veritatis
+              sit nulla.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header img"></img>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
